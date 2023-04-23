8ecbd173bf1316aa60a2250c59ea65567b6e9bca
YSidorko <yury.sidorko@gmail.com>
2023-04-24 00:40:06 +0200

    - jw updates

--- index.js
+++ index.js
@@ -4,13 +4,30 @@ const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 const WEEK = DAY * 7;
 
-const MAIN_DIR = process?.env.INIT_CWD || process?.env.PWD;
+const MAIN_DIR = process.env.INIT_CWD || process.env.PWD;
+const DEFAULT_LAYOUT = 'index';
 
 const STORE_TYPES = {
   FS_STORE: 'fs',
   MONGODB_STORE: 'mongodb'
 };
 Object.freeze(STORE_TYPES);
+
+const RESERVED_URLS = {
+  PAGE_LOGIN: '/login',
+  PAGE_LOGOUT: '/logout',
+  PAGE_ADMIN: '/admin',
+
+  MEMORY_ROUTE: '/__memory'
+};
+Object.freeze(RESERVED_URLS);
+
+const DATA_TYPES = {
+  TYPE_ENVIRONMENT: 'environment',
+  TYPE_WORKER: 'worker',
+  TYPE_LOG: 'log'
+};
+Object.freeze(DATA_TYPES);
 
 module.exports = {
   SECOND,
@@ -19,7 +36,10 @@ module.exports = {
   DAY,
   WEEK,
 
+  ...DATA_TYPES,
   ...STORE_TYPES,
+  ...RESERVED_URLS,
 
-  MAIN_DIR
+  MAIN_DIR,
+  DEFAULT_LAYOUT
 };
