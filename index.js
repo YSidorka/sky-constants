0a27028186783597c906d3800c95cd4a4c6cb6db
YSidorko <yury.sidorko@gmail.com>
2023-05-29 23:18:56 +0200

    - feedback service consts

--- index.js
+++ index.js
@@ -6,11 +6,31 @@ const WEEK = DAY * 7;
 
 const MAIN_DIR = process?.env.INIT_CWD || process?.env.PWD;
 
+const DEFAULT_DELIMITER = '###';
+const DEFAULT_REGEX_DELIMITER = '###';
+
+const DATA_TYPES = {
+  TYPE_COMPONENT: 'component',
+  TYPE_DOMAIN: 'domain'
+};
+Object.freeze(DATA_TYPES);
+
+const RESERVED_URLS = {
+  PAGE_API: '/api',
+  MEMORY_ROUTE: '/__memory'
+};
+Object.freeze(RESERVED_URLS);
+
 const STORE_TYPES = {
   FS_STORE: 'fs',
   MONGODB_STORE: 'mongodb'
 };
 Object.freeze(STORE_TYPES);
+
+function isValidType(type) {
+  if (!type) return false;
+  return Object.values(DATA_TYPES).includes(type);
+}
 
 module.exports = {
   SECOND,
@@ -19,6 +39,15 @@ module.exports = {
   DAY,
   WEEK,
 
+  $DD: DEFAULT_DELIMITER,
+  $REGDD: DEFAULT_REGEX_DELIMITER,
+
+  DATA_TYPES,
+  ...DATA_TYPES,
+  isValidType,
+
+  ...RESERVED_URLS,
+  
   ...STORE_TYPES,
 
   MAIN_DIR
